8dd12e8de12cd2c3b9f4163ee7683578f7ae58c5
brand <brand@CRISTOPHE_M_L>
2024-06-07 16:36:19 -0400

    Agregar archivos de proyecto.

---
+++ Tabla de multiplicacion/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace tabla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n Esto es un programa para saber la tabla de multilicasion\n ");
+
+            Console.WriteLine("\n Ingrese un Numero de la tabla a Saber\n");
+            int Nume1 = int.Parse(Console.ReadLine());
+
+            int Num = 0;
+            while (Num <= 12)
+            {
+                Console.WriteLine($" {Nume1} x {Num} = { Nume1 * Num}");
+                Num++; 
+            }
+
+        }
+    }
+}
